0517dbea052592a1bb00f04490fac3de6452a9b9
Emerson MX <emersonmx@gmail.com>
2012-12-20 01:11:15 -0300

    Fixed typo on project name in license.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

--- src/mxgame/system/time/system_time.cpp
+++ src/mxgame/system/time/system_time.cpp
@@ -3,20 +3,20 @@
 
   Author: Emerson Max de Medeiros Silva <emersonmx@gmail.com>
 
-  This file is part of pong.
+  This file is part of mxgame.
 
-  pong is free software: you can redistribute it and/or modify
+  mxgame is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  pong is distributed in the hope that it will be useful,
+  mxgame is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with pong.  If not, see <http://www.gnu.org/licenses/>.
+  along with mxgame.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <cstdlib>
