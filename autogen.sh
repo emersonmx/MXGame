9f4f603ac75f9ec26ae7e4446fdce26b62a4f6cc
Emerson MX <emersonmx@gmail.com>
2013-05-31 04:49:45 -0300

    Modifying the "autogen.sh" to run sub-autogens.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,20 @@
 #! /bin/sh
 
+# Call sub-autogens
+SUBDIRS=
+
+for dir in $SUBDIRS
+do
+    cd $dir
+    echo "===== $(pwd)/autogen.sh ====="
+    ./autogen.sh
+    cd ..
+done
+
+# Generates project files autotools.
+echo "===== $(pwd)/autogen.sh ====="
 mkdir -p m4
 set -e
 autoreconf --install --force
 rm -rf autom4te.cache
+
