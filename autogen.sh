027170c7e28781c1138f4ce018eb7ee61c9d123c
Emerson MX <emersonmx@gmail.com>
2013-10-21 21:17:05 -0300

    Cleaning autogen.sh and putting AC_CONFIG_FILES close to AC_OUTPUT.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,27 +1,10 @@
 #! /bin/sh
 
-SUBDIRS=
+rm -rf autom4te.cache/
+rm -rf build-aux/
+rm -rf m4/
 
-for dir in $SUBDIRS
-do
-    cd $dir
-    ./autogen.sh bare
-    cd ..
-done
-
-if [ $# == 0 ]
-then
-    mkdir -p m4
-    set -e
-    autoreconf --install --force
-    rm -rf autom4te.cache
-else
-    if [ $1 == "bare" ]
-    then
-        mkdir -p m4
-        rm -rf autom4te.cache
-    else
-        echo "Usage: $0 [bare]"
-    fi
-fi
+mkdir -p m4
+set -e
+autoreconf --install --force .
 
